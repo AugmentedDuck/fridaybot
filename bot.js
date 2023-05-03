6e214a5534951ffff79a62cbb6c7b4fc608b0866
AugmentedDuck <anders.m.matzen@gmail.com>
2023-05-03 17:37:38 +0200

    Event handler

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 //Debug, just to see if the script runs
 console.log(`Bip bop i am a bot!`);
 
-
 // Require the necessary discord.js classes
 const fs = require(`node:fs`)
 const path = require(`node:path`)
@@ -11,9 +10,8 @@ const { token } = require('./config.json');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-
 //Dynamically retrive the command files
+client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands'); //Where are the commands located
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -34,39 +32,19 @@ for (const folder of commandFolders) {
 	}
 }
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+//Event handler
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.on(Events.InteractionCreate, async interaction => {
-  //If not a slash command
-  if (!interaction.isChatInputCommand()) { 
-    return; 
-  }
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  //If it's an invalid command
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found`);
-    return
-  }
-  
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: `There was an error wile executing this command`, ephemeral: true });
-    } else {
-      await interaction.reply({ content: `There was an error wile executing this command`, ephemeral: true });
-    }
-  }
-
-  console.log(interaction);
-})
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // Log in to Discord with your client's token
 client.login(token);
