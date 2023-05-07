d37ee58c7fde3b8cb35e414af177842d0abbca1d
AugmentedDuck <anders.m.matzen@gmail.com>
2023-05-07 13:20:04 +0200

    Added an image scraper

---
+++ commands/utility/image.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require(`discord.js`)
+var Scraper = require('images-scraper');
+
+const google = new Scraper({
+  puppeteer: {
+    headless: false, //True takes to long :(
+  },
+});
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName(`image`)
+    .setDescription(`search for an image`)
+    .addStringOption(option =>
+      option.setName('query')
+        .setDescription('The search word(s)')
+        .setRequired(true)
+      ),
+    
+  async execute(interaction) {
+    var searchWord = interaction.options.getString('query');
+
+    await interaction.reply(`Searching for ${searchWord}...`); //Send a response to Discord !! NEEDED FOR TIMEOUT DO NOT REMOVE !!
+    const results = await google.scrape(searchWord, 1);
+    await interaction.editReply(results[0].url)
+  },
+};
