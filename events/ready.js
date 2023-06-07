47bd88971a8f485996101a420f977789c09b610f
AugmentedDuck <anders.m.matzen@gmail.com>
2023-06-07 21:17:15 +0200

    Added birthdays + when it is friday

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,30 @@
 const { Events } = require('discord.js');
+const birthday = require('./birthday.js')
 
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);		
+		const todayDate = new Date().getDate();
+		const channelName = '876766059821150242'
+
+		let message = birthday.isToday(channelName, todayDate)
+
+		if (message != null) {
+			client.channels.cache.get(channelName)
+			.send(message);
+		}
+
+		if ((new Date()).getDay() === 5) {
+			setTimeout(() => {
+					client.channels.cache.get(channelName)
+							.send('https://imgur.com/a/5XZocEX')
+			}, 500);
+		}
+
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+
 	},
 };
 
