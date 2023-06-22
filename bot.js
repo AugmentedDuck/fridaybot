4697a4291e2762b0702f6d083acf03d9e0a79353
AugmentedDuck <anders.m.matzen@gmail.com>
2023-06-22 14:39:48 +0200

    Moved birthday/Friday response to main channel
    To make it able to run non-stop

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const birthday = require('../.data/birthday.js')
+
 //Debug, just to see if the script runs
 console.log(`Bip bop i am a bot!`);
 
@@ -48,3 +50,46 @@ for (const file of eventFiles) {
 
 // Log in to Discord with your client's token
 client.login(token);
+
+// checks if one day has passed. 
+function hasOneDayPassed(){
+  // get today's date. eg: "7/37/2007"
+  var date = new Date().getDay();
+
+  // if there's a date in localstorage and it's equal to the above: 
+  // inferring a day has yet to pass since both dates are equal.
+  if( yesterday == date ) {
+		return false;
+	}
+
+  // this portion of logic occurs when a day has passed
+  yesterday = date;
+  return true;
+}
+
+
+// some function which should run once a day
+function runOncePerDay(){
+  if( !hasOneDayPassed() ) return false;
+
+  // your code below
+  const channelName = '876766059821150242'
+
+		let message = birthday.isToday()
+
+		if (message != null) {
+			client.channels.cache.get(channelName)
+			.send(message);
+		}
+
+		if ((new Date()).getDay() === 5) {
+			setTimeout(() => {
+					client.channels.cache.get(channelName)
+							.send('https://imgur.com/a/5XZocEX')
+			}, 500);
+		}
+}
+
+runOncePerDay();
+let dayInMilliseconds = 1000 * 60 * 60 * 24;
+setInterval(runOncePerDay, dayInMilliseconds);
--- events/ready.js
+++ events/ready.js
@@ -1,29 +1,11 @@
 const { Events } = require('discord.js');
-const birthday = require('../.data/birthday.js')
 
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		const channelName = '876766059821150242'
-
-		let message = birthday.isToday()
-
-		if (message != null) {
-			client.channels.cache.get(channelName)
-			.send(message);
-		}
-
-		if ((new Date()).getDay() === 5) {
-			setTimeout(() => {
-					client.channels.cache.get(channelName)
-							.send('https://imgur.com/a/5XZocEX')
-			}, 500);
-		}
-
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-
 	},
 };
 
