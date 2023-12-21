0a4f2945a056157877aae643a5a0a30337366c4d
AugmentedDuck <anders.m.matzen@gmail.com>
2023-12-21 16:10:10 +0100

    Added exam countdown

--- commands/fun/date.js
+++ commands/fun/date.js
@@ -3,10 +3,26 @@ const { SlashCommandBuilder } = require(`discord.js`)
 module.exports = {
   data: new SlashCommandBuilder()
     .setName(`date`)
-    .setDescription(`Sends the current date`),
+    .setDescription(`Sends amount of time to exams`),
 
   async execute(interaction) {
-    const currentDate = new Date().getDate() + " / " + new Date().getMonth(); //How long did it take?
-    await interaction.reply(`The date is: ${currentDate}`)
+    await interaction.reply(`Calculating`)
+    let today = new Date()
+		let nowUNIX = Math.round(today.getTime() / 1000)
+		const examDateUNIX = 1716384300 
+		let timeToExamsSeconds = examDateUNIX - nowUNIX
+		const secondsInDay = 60 * 60 * 24
+		let timeToExamsDays = Math.round(timeToExamsSeconds / secondsInDay)
+		await interaction.editReply(alertToDays(timeToExamsDays)) 
+    
+		function alertToDays(daysBetween) {
+      const channelName = '876766059821150242'
+
+			let schoolDays = Math.round( daysBetween * ( 5 / 7) - ( 37 * (daysBetween / 152)) ) 
+
+			console.log(daysBetween,schoolDays, "days")
+
+      return `There is ${daysBetween} days left\nThat is ${schoolDays}\* school days left\n\*this is an estimation and may be wrong`
+		}
   },
 };
--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, time } = require('discord.js');
 const birthday = require('../.data/birthday.js')
 
 
@@ -8,48 +8,51 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		let yesterday = null;
-		
-		// checks if one day has passed. 
-		function hasOneDayPassed(){
-			// get today's date. eg: "7/37/2007"
-			var date = new Date().getDay();
-		
-			// if there's a date in localstorage and it's equal to the above: 
-			// inferring a day has yet to pass since both dates are equal.
-			if( yesterday == date ) {
-				return false;
-			}
-		
-			// this portion of logic occurs when a day has passed
-			yesterday = date;
-			return true;
-		}
-		
-		// some function which should run once a day
+
 		function runOncePerDay(){
-			if( !hasOneDayPassed() ) return false;
-		
-			// your code below
 			const channelName = '876766059821150242'
 		
-				let message = birthday.isToday()
+			let message = birthday.isToday()
 		
-				if (message != null) {
-					client.channels.cache.get(channelName)
-					.send(message);
+			if (message != null) {
+				client.channels.cache.get(channelName)
+				.send(message);
+			}
+				
+			if ((new Date()).getDay() === 5) {
+				setTimeout(() => {
+					client.channels.cache.get(channelName).send('https://imgur.com/a/5XZocEX')
+				}, 500);
+			}
+
+			let today = new Date()
+			let nowUNIX = Math.round(today.getTime() / 1000)
+			const examDateUNIX = 1716384300 
+			let timeToExamsSeconds = examDateUNIX - nowUNIX
+			const secondsInDay = 60 * 60 * 24
+			let timeToExamsDays = Math.round(timeToExamsSeconds / secondsInDay)
+			alertToDays(timeToExamsDays)
+		}
+
+		function alertToDays(daysBetween) {
+			const channelName = '876766059821150242'
+
+			let schoolDays = Math.round( daysBetween * ( 5 / 7) - ( 37 * (daysBetween / 152)) ) 
+
+			console.log(daysBetween,schoolDays, "days")
+
+			if (daysBetween == 100) {
+				client.channels.cache.get(channelName).send(`There is 100 days left to exams\n that is an estimated ${schoolDays}`)
+			} else if (daysBetween <= 10) {
+				client.channels.cache.get(channelName).send(`There is ${daysBetween} days left to exams\n that is an estimated ${schoolDays}`)
+			} else if (daysBetween < 100) {
+				if (daysBetween % 10 == 0) {
+				client.channels.cache.get(channelName).send(`There is ${daysBetween} days left to exams\n that is an estimated ${schoolDays}`)
 				}
-		
-				if ((new Date()).getDay() === 5) {
-					setTimeout(() => {
-							client.channels.cache.get(channelName).send('https://imgur.com/a/5XZocEX')
-					}, 500);
-				}
+			}
 		}
 		
-		runOncePerDay();
-		let dayInMilliseconds = 1000 * 60 * 60 * 24;
-		setInterval(runOncePerDay, dayInMilliseconds);		
+		runOncePerDay();	
 	},
 };
 
