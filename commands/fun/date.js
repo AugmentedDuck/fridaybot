62e3dd005093ffdc7370811b00184bf061ff418e
AugmentedDuck <anders.m.matzen@gmail.com>
2024-05-23 18:56:47 +0200

    Removed countdown

--- commands/fun/date.js
+++ commands/fun/date.js
@@ -7,22 +7,21 @@ module.exports = {
 
   async execute(interaction) {
     await interaction.deferReply()
+	await interaction.editReply('This command is no longer applicable')
     let today = new Date()
 		let nowUNIX = Math.round(today.getTime() / 1000)
 		const examDateUNIX = 1716384300 
 		let timeToExamsSeconds = examDateUNIX - nowUNIX
 		const secondsInDay = 60 * 60 * 24
 		let timeToExamsDays = Math.round(timeToExamsSeconds / secondsInDay)
-		await interaction.editReply(alertToDays(timeToExamsDays)) 
+		//await interaction.editReply(alertToDays(timeToExamsDays)) 
     
 		function alertToDays(daysBetween) {
-      const channelName = '876766059821150242'
-
 			let schoolDays = Math.round( daysBetween * ( 5 / 7) - ( 37 * (daysBetween / 152)) ) 
 
 			console.log(daysBetween,schoolDays, "days")
 
-      return `There is ${daysBetween} days left\nThat is ${schoolDays}\* school days left\n\*this is an estimation and may be wrong`
+      		return `There is ${daysBetween} days left\nThat is ${schoolDays}\* school days left\n\*this is an estimation and may be wrong`
 		}
   },
 };
--- events/ready.js
+++ events/ready.js
@@ -24,30 +24,6 @@ module.exports = {
 					client.channels.cache.get(channelName).send('https://imgur.com/a/5XZocEX')
 				}, 500);
 			}
-
-			alertToDays()
-		}
-
-		function alertToDays() {
-			let today = new Date()
-			let nowUNIX = Math.round(today.getTime())
-			const examDateUNIX = 1716384300000
-			console.log(nowUNIX)
-			let timeToExamsSeconds = examDateUNIX - nowUNIX
-			const millisecondsInDay = 60 * 60 * 24 * 1000
-			let daysBetween = Math.round(timeToExamsSeconds / millisecondsInDay)
-
-			const channelName = '876766059821150242'
-
-			let schoolDays = Math.round( daysBetween * ( 5 / 7) - ( 28 * (daysBetween / 139)) ) 
-
-			console.log(daysBetween,schoolDays, "days")
-
-			if (daysBetween <= 10) {
-				client.channels.cache.get(channelName).send(`There is a total of ${daysBetween} days left to exams\nThat is an estimated ${schoolDays} days in school`)
-			} else if (daysBetween % 10 == 0) {
-				client.channels.cache.get(channelName).send(`There is a total of ${daysBetween} days left to exams\nThat is an estimated ${schoolDays} days in school`)
-			}
 		}
 		
 		runOncePerDay();	
