898a8b8ab7a4dad8952795ab8efc8b50bbaa08dd
AugmentedDuck <anders.m.matzen@gmail.com>
2024-12-31 00:55:24 +0100

    Music now works

--- commands/fun/music.js
+++ commands/fun/music.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, ChannelType } = require(`discord.js`)
-const { AudioPlayerStatus, joinVoiceChannel, createAudioPlayer, createAudioResource, VoiceConnectionStatus } = require('@discordjs/voice');
-const play = require('play-dl');
+const { AudioPlayerStatus, joinVoiceChannel, createAudioPlayer, createAudioResource, VoiceConnectionStatus, StreamType } = require('@discordjs/voice');
+const ytdl = require('youtube-dl-exec');
+const fs = require('fs');
 
 let queue = [];
 let voiceChannel;
@@ -10,178 +11,124 @@ module.exports = {
   data: new SlashCommandBuilder()
   .setName(`music`)
   .setDescription(`Control music in voice chat`)
-
   .addSubcommand(subcommand =>
-		subcommand.setName('play')
-			.setDescription('Put a song in queue !WARNING SPOTIFY IS NOT FULLY SUPPORTED!')
-			.addChannelOption((option) =>
-        option.setName('channel')
-          .setDescription('Channel to join')
-          .setRequired(true)
-          .addChannelTypes(ChannelType.GuildVoice))
-      .addStringOption((option) =>
-        option.setName('query')
-          .setDescription('The search word(s)')
-          .setRequired(true)))
-
+		  subcommand.setName('play')
+			          .setDescription('Put a song in queue !WARNING SPOTIFY IS NOT FULLY SUPPORTED!')
+			          .addChannelOption((option) =>
+                  option.setName('channel')
+                        .setDescription('Channel to join')
+                        .setRequired(true)
+                        .addChannelTypes(ChannelType.GuildVoice))
+                .addStringOption((option) =>
+                  option.setName('query')
+                        .setDescription('The search word(s)')
+                        .setRequired(true)))
   .addSubcommand(subcommand =>
-    subcommand.setName('stop')
-      .setDescription('Stop the song and clear queue'))
-
+      subcommand.setName('stop')
+                .setDescription('Stop the song and clear queue'))
   .addSubcommand(subcommand =>
-    subcommand.setName('clear')
-      .setDescription('Clear the queue'))
-
+      subcommand.setName('clear')
+                .setDescription('Clear the queue'))
   .addSubcommand(subcommand =>
-    subcommand.setName('pause_resume')
-      .setDescription('Pause or resume the music'))
-      
-      .addSubcommand(subcommand =>
-        subcommand.setName('skip')
-        .setDescription('Skips current song'))
+      subcommand.setName('pause_resume')
+                .setDescription('Pause or resume the music'))
+  .addSubcommand(subcommand =>
+      subcommand.setName('skip')
+                .setDescription('Skips current song'))
         
-        .addSubcommand(subcommand =>
-          subcommand.setName('queue')
-          .setDescription('View the current queue'))
+  .addSubcommand(subcommand =>
+      subcommand.setName('queue')
+                .setDescription('View the current queue'))
           
-          .addSubcommand(subcommand =>
-            subcommand.setName('shuffle')
-      .setDescription('Shuffles the current queue'))
+  .addSubcommand(subcommand =>
+      subcommand.setName('shuffle')
+                .setDescription('Shuffles the current queue'))
       ,
-      //------------------------------------------------------------------------------------------------------------------
-      
   async execute(interaction) {
-    
-    /////////////////////////////////////////////////////
-    //
-    // DOES NOT WORK ANYMORE
-    //
-    /////////////////////////////////////////////////////
 
-    await interaction.reply("This command is not working anymore")
+    //////////////////////////////////////////////////////////////
+    //
+    // EXECUTE INTERACTION
+    //
+    //////////////////////////////////////////////////////////////
 
-    /*
     if (interaction.options.getSubcommand() === 'play') {
       voiceChannel = interaction.options.getChannel('channel')
       const searchWord = interaction.options.getString('query')
-          
+
       await interaction.deferReply();
-          
-      const playNextSong = async () => {
-        if(queue.length !== 0) {
-          
-          const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: interaction.guildId,
-            adapterCreator: interaction.guild.voiceAdapterCreator
-          })
 
-          if(queue[0]){
-            //const stream = ytdl(queue[0].url, {filter: 'audioonly'})
-            const streamMusic = await play.stream(queue[0].url, {discordPlayerCompatibility: true});
-            const resource = createAudioResource(streamMusic.stream, { inputType : streamMusic.type })
-            player.play(resource);
-            interaction.editReply(`Now playing: ***${queue[0].title}***, in ${voiceChannel} \nQueue length: ${queue.length}`);
-          }
-              
-          const subscribtion = connection.subscribe(player);
+      player.on(AudioPlayerStatus.Idle, async () => {
+        let isNew = false
+        
+        try {
+          fs.rmSync('./currentSong.mp3');
+          isNew = true
+        } catch (error) {
+          isNew = false
         }
-      }
-          
-      const videoFinder = async (query) => {
-        //const videoResult = await ytSearch(query)
 
-        if(play.yt_validate(query) == 'video' && query.startsWith('https')){ //YOUTUBE VIDEO
-          const videoResult = await play.video_basic_info(query)
-          await interaction.editReply(`Added ***${videoResult.video_details.title}*** to queue\nQueue length: ${queue.length}`)
-          
-          return videoResult.video_details;
-        } else if (play.yt_validate(query) == 'playlist' && query.startsWith('https')) { //YOUTUBE PLAYLIST
-          const playList = await play.playlist_info(query, { incomplete : true } );
-          let count = 0
-
-          for (let i = 0; i < playList.videos.length; i++){
-            queue.push(playList.videos[i])
-            count++
+        if (isNew) {
+          let response = queue.shift();
+          console.log(queue)
+          if (response != undefined) {
+            playSong();
           }
-
-          await interaction.editReply(`Added ***${count}*** songs from **${playList.title}** to queue\nQueue length: ${queue.length}`)
-          
-          if(queue.length == count) {
-            playNextSong();
-          }
-
-          return { url: "https://www.youtube.com/watch?v=jhFDyDgMVUI", title: "return", needeToWork: true } //ONE SECOND EMPTY VIDEO
-
-        } else if (play.sp_validate(query) == 'track') { //SPOTIFY TRACK
-          const song = await play.spotify(query)
-          if (song == null) {
-            await interaction.editReply(`Spotify not working: <@535841235064324106> Update you API token!`)
-          }
-          const videoResult = await videoFinder(song.name);
-
-          return videoResult
-        } else if (play.sp_validate(query) == 'album' || play.sp_validate(query) == 'playlist') { //SPOTIFY ALBUM
-          const playlist = await play.spotify(query)
-          const playlistSongs = playlist.fetched_tracks.get('1');
-
-          let count = 0
-
-          for (let i = 0; i < playlistSongs.length; i++){
-            let song = await videoFinder(playlistSongs[i].name)
-            queue.push(song)
-            count++
-          }
-          
-          await interaction.editReply(`Added ***${count}*** songs from **${playlist.name}** to queue\nQueue length: ${queue.length}`)
-
-          if(queue.length == count) {
-            playNextSong();
-          }
-
-          return { url: "https://www.youtube.com/watch?v=jhFDyDgMVUI", title: "return", needeToWork: true } //ONE SECOND EMPTY VIDEO
-        } else { //SEARCH FOR VIDEO ON YOUTUBE
-          const videoResult = await play.search(query, { limit: 1 });
-
-          await interaction.editReply(`Added ***${videoResult[0].title}*** to queue\nQueue length: ${queue.length}`)
-          
-          return videoResult[0];
         }
-      }
-      
-      const video = await videoFinder(searchWord)
-      if (!video.needeToWork) {
-        queue.push(video);
-      }
-         
-      
-      
-      if (queue.length == 1) {
-        await playNextSong();
+      });
+
+      async function playSong() {
+        console.log("Playing song")
+        const stream = await ytdl(queue[0], {extractAudio: true, audioFormat: 'mp3', output: 'currentSong'});
+
+        const resource = createAudioResource('./currentSong.mp3', { inputType: StreamType.Arbitrary });
+
+        player.play(resource);
+        
+        
+        const connection = joinVoiceChannel({
+          channelId: voiceChannel.id,
+          guildId: interaction.guildId,
+          adapterCreator: interaction.guild.voiceAdapterCreator
+        });
+
+        const subscribe = connection.subscribe(player);
       }
 
+      async function addToQueue() {
+        if (searchWord != undefined || searchWord != '') {
+          // Check if the search word is a youtube link
+          const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/g;
+          const match = searchWord.match(regex);
+          if (match) {
+            queue.push(searchWord)
 
-      player.on("stateChange", (oldState, newState) => {
-        if (newState.status === AudioPlayerStatus.Idle) { 
-          if(queue.length > 1) {
-            queue.shift();
-            playNextSong();
+            if (queue.length == 1) {
+              await interaction.editReply("Playing song...")
+              playSong();
+            } else {
+              await interaction.editReply("Added to queue")
+              console.log(queue)
+            }
+
           } else {
-            const connection = joinVoiceChannel({
-              channelId: voiceChannel.id,
-              guildId: interaction.guildId,
-              adapterCreator: interaction.guild.voiceAdapterCreator
-            })
-            
-            const subscribtion = connection.subscribe(player);
-  
-            subscribtion.unsubscribe();
+            await interaction.editReply("NOT A VALID YOUTUBE LINK")
+            return
           }
-
         }
-      })
+      }
+
+      addToQueue();
       
     } else if (interaction.options.getSubcommand() === 'stop'){
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // STOP THE PLAYER AND CLEAR THE QUEUE
+      //
+      //////////////////////////////////////////////////////////////////////////
+
+
       queue = [];
       player.stop();
       await interaction.reply("Stopped playing")
@@ -197,10 +144,24 @@ module.exports = {
       subscribtion.unsubscribe();
 
     } else if (interaction.options.getSubcommand() === 'clear'){
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // CLEAR THE QUEUE
+      //
+      //////////////////////////////////////////////////////////////////////////
+
       queue = [];
       await interaction.reply("Cleared queue")
 
     } else if (interaction.options.getSubcommand() === 'pause_resume'){
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // PAUSE OR RESUME THE PLAYER
+      //
+      //////////////////////////////////////////////////////////////////////////
+
       if (player.state.status === AudioPlayerStatus.Paused) {
         player.unpause()
         await interaction.reply("Resuming...")
@@ -211,44 +172,29 @@ module.exports = {
       }
 
     } else if (interaction.options.getSubcommand() === 'skip'){
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // SKIP THE CURRENT SONG
+      //
+      //////////////////////////////////////////////////////////////////////////
+
       if (player.state.status === AudioPlayerStatus.Playing) {
-        queue.shift();
         await interaction.reply("Skipping song...")
         
-        if(queue[0]){
-          //const stream = ytdl.stream(queue[0].url, {filter: 'audioonly'})
-          const streamMusic = await play.stream(queue[0].url, {discordPlayerCompatibility: true});
-          const resource = createAudioResource(streamMusic.stream, { inputType : streamMusic.type })
-          player.play(resource);
-          interaction.editReply(`Now playing: ***${queue[0].title}***, in ${voiceChannel}`);
-
-          const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: interaction.guildId,
-            adapterCreator: interaction.guild.voiceAdapterCreator
-          })
+        player.stop();
           
-          const subscribtion = connection.subscribe(player);
-        } else {
-          interaction.editReply(`No songs left, leaving`)
-          player.stop();
-          
-          const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: interaction.guildId,
-            adapterCreator: interaction.guild.voiceAdapterCreator
-          })
-          
-          const subscribtion = connection.subscribe(player);
-
-          subscribtion.unsubscribe();
-        }
-
       } else {
         await interaction.reply("No song is playing")
       } 
     } else if (interaction.options.getSubcommand() === 'queue'){
       
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // VIEW THE CURRENT QUEUE
+      //
+      //////////////////////////////////////////////////////////////////////////
+
       if (queue.length == 0) {
         await interaction.reply("No songs in queue")
         return
@@ -264,6 +210,13 @@ module.exports = {
       await interaction.editReply("The current queue is:\n" + queueString);
 
     } else if (interaction.options.getSubcommand() === 'shuffle'){
+      
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // SHUFFLE THE QUEUE
+      //
+      //////////////////////////////////////////////////////////////////////////
+      
       await interaction.reply("Shuffling...")
       
       queue.shift();
@@ -274,6 +227,5 @@ module.exports = {
       }
       await interaction.editReply("Shuffled the queue");
     }
-    */
   },
 };
